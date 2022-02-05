e33fc584767849c39d6a47ab7b0940e9548f4b38
Akosh Poemandres <arjay.jones@nlucis.dev>
2022-02-05 14:31:03 -0800

    added number prompt from section 4

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 int main() {
-	std::cout << "What's kickin' Sky Chicken?" << std::endl;
+	int num;
+	std::cout << "What's kickin' Sky Chicken? Enter your favorite number..." << std::endl;
+	std::cin >> num;
+	std::cout << "Oh cool! That's a good one." << std::endl;
 	return 0;
 }
 
