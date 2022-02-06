7cfc98cd79240e5d0f5e8bbc3962abe77594d5c5
Akosh Poemandres <arjay.jones@nlucis.dev>
2022-02-06 13:22:26 -0800

    Added simple conditional for if input matches faveNum.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+
+// Qualified namespace
+using std::cout;
+using std::cin;
+
+// Global namespace
 using namespace std;
 
 // Gets int number variable from Section4.cpp
@@ -8,7 +14,12 @@ int main() {
 	int num;
 	cout << "What's kickin' Sky Chicken? Enter your favorite number...:";
 	cin >> num;
-	cout << "Oh cool! " << num << " is a good one. My favorite number is " << faveNum << "."  << endl;
+	if (num != faveNum) {
+		cout << "Oh cool! " << num << " is a good one. My favorite number is " << faveNum << "."  << endl;
+	}
+	else {
+		cout << "No way! That's my favorite number too! Seriously! " << faveNum << " is my favorite number!" << endl;
+	}
 	return 0;
 }
 
