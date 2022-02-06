22a692b863755728cd144481487c8b158d2b9ce3
Akosh Poemandres <arjay.jones@nlucis.dev>
2022-02-06 11:21:18 -0800

    Added namesspace for STL

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+using namespace std;
 
+// Gets int number variable from Section4.cpp
 extern int number;
 
 int main() {
 	int num;
-	std::cout << "What's kickin' Sky Chicken? Enter your favorite number...:";
-	std::cin >> num;
-	std::cout << "Oh cool! " << num << " is a good one. My favorite number is " << number << std::endl;
+	cout << "What's kickin' Sky Chicken? Enter your favorite number...:";
+	cin >> num;
+	cout << "Oh cool! " << num << " is a good one. My favorite number is " << number << endl;
 	return 0;
 }
 
