2dc605698a7a6566894255508fa546a80da05f1e
Akosh Poemandres <arjay.jones@nlucis.dev>
2022-02-06 11:49:10 -0800

    Fixed linker error caused by forgetting to save lol

--- Section4.cpp
+++ Section4.cpp
@@ -1,3 +1,2 @@
 #pragma once
-
-int number = 36;
+int faveNum = 36;
--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 // Gets int number variable from Section4.cpp
-extern int number;
+extern int faveNum;
 
 int main() {
 	int num;
 	cout << "What's kickin' Sky Chicken? Enter your favorite number...:";
 	cin >> num;
-	cout << "Oh cool! " << num << " is a good one. My favorite number is " << number << endl;
+	cout << "Oh cool! " << num << " is a good one. My favorite number is " << faveNum << "."  << endl;
 	return 0;
 }
 
