3052c628737856e85e01d3c33b58bec518a4f66f
Akosh Poemandres <arjay.jones@nlucis.dev>
2022-02-06 19:24:04 -0800

    added a favorite number function

--- Section4.cpp
+++ Section4.cpp
@@ -1,2 +1,6 @@
 #pragma once
-int faveNum{ 36 }; // Cpp11 List Initialization Syntax (LIS)
+int faveNum { 36 }; // Cpp11 List Initialization Syntax (LIS)
+
+static int getFavoriteNumber (int localFavoriteNumber) {
+	return localFavoriteNumber ? localFavoriteNumber : faveNum;
+}
